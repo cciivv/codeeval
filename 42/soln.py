85ff82314dac020b5f13d100b211e6ebbc5b9658
Carl Vanderlip <carl.i.vanderlip@gmail.com>
2015-04-01 16:39:35 -0700

    42: no cache
    remove cache of previously computed sequences (testing to see
    differences in memory usage vs speed)
    
    result: halved execution time, but *increased* memory usage by
    ~4x... not what I expected, but it could be due to the
    increased number of recursions required and sloppy gc?

--- 42/soln.py
+++ 42/soln.py
@@ -43,17 +43,12 @@ def add_mt(left, right):
         #add_def_zero(mt, r, right[r]);
     return mt;
     
-def mt_val(n, cache, mod_val):
+def mt_val(n, mod_val):
     if len(n) == 0:
         return None;
     num = int(n);
-    if num not in cache:
-        temp_mt = {(num%mod_val): 1};
-        cache[num] = temp_mt;
-    #else:
-        #print("\t\t\t\t\t\tcache HIT");
-    #print("val = ", cache[num]);
-    return cache[num];
+    temp_mt = {(num%mod_val): 1};
+    return temp_mt;
 
 def left(A):
     return A[:(len(A)//2)]
@@ -75,42 +70,39 @@ def window_bounds(size, total_len, split):
         windows.append((front(i), back(i)));
     return windows;
     
-def mt_seq(seq, cache, mods_gcm):
-    if ''.join(seq) not in cache:
-        if len(seq) == 0:
-            return {};
-        if len(seq) == 1:
-            return mt_val(seq[0], cache, mods_gcm);
-        lft = left(seq);
-        #combination of the left and right halves possible values
-        combo_table = combo_mt(mt(lft, cache, mods_gcm), mt(right(seq), cache, mods_gcm),mods_gcm);
-        #add in mod value for the whole sequence as a value
-        coverall_table = mt(''.join(seq), cache, mods_gcm);
-        combo_table = add_mt(combo_table, coverall_table);
-        #then add in all the combinations when the two halves are combined
-        for window_size in range(2,len(seq)):
-            windows = window_bounds(window_size, len(seq), len(lft));
-            for window in windows:
-                before_table = mt(seq[:window[0]],cache, mods_gcm);
-                during_table = mt(''.join(seq[window[0]:window[1]+1]), cache, mods_gcm);
-                temp_combo = combo_mt(before_table, during_table, mods_gcm);
-                after_table = mt(seq[window[1]+1:], cache, mods_gcm); 
-                temp_table = combo_mt(temp_combo, after_table, mods_gcm);
-                combo_table = add_mt(combo_table, temp_table);
-        cache[''.join(seq)] = combo_table;
-    return cache[''.join(seq)];
+def mt_seq(seq, mods_gcm):
+    if len(seq) == 0:
+        return {};
+    if len(seq) == 1:
+        return mt_val(seq[0], mods_gcm);
+    lft = left(seq);
+    #combination of the left and right halves possible values
+    combo_table = combo_mt(mt(lft, mods_gcm), mt(right(seq), mods_gcm),mods_gcm);
+    #add in mod value for the whole sequence as a value
+    coverall_table = mt(''.join(seq), mods_gcm);
+    combo_table = add_mt(combo_table, coverall_table);
+    #then add in all the combinations when the two halves are combined
+    for window_size in range(2,len(seq)):
+        windows = window_bounds(window_size, len(seq), len(lft));
+        for window in windows:
+            before_table = mt(seq[:window[0]], mods_gcm);
+            during_table = mt(''.join(seq[window[0]:window[1]+1]), mods_gcm);
+            temp_combo = combo_mt(before_table, during_table, mods_gcm);
+            after_table = mt(seq[window[1]+1:], mods_gcm); 
+            temp_table = combo_mt(temp_combo, after_table, mods_gcm);
+            combo_table = add_mt(combo_table, temp_table);
+    return combo_table;
 
-def mt(seq, cache, mods_gcm):
+def mt(seq, mods_gcm):
     """ 
     seq: a list of individual digits 
-    cache: dict of cached modtables for digit span (represented as a join of digits)
     mods: numbers to check mods of
     """
-    cases = {list: lambda t,c,p: mt_seq(t,c,p), \
-            str: lambda t,c,p: mt_val(t,c,p)};
+    cases = {list: lambda t,p: mt_seq(t,p), \
+            str: lambda t,p: mt_val(t,p)};
     #check for empty sequence or empty string?
     try:
-        mt = cases[type(seq)](seq,cache,mods_gcm);
+        mt = cases[type(seq)](seq,mods_gcm);
         return mt;
     except KeyError:
         print("KeyError");
@@ -122,13 +114,12 @@ if (len(sys.argv) <= 1):
 else:
     with open(sys.argv[1]) as f:
         num_lists = [list(line.rstrip()) for line in f];
-    cache = {};
     primes = [2,3,5,7];
     gcm = 1;
     for p in primes:
         gcm *= p;
     for sequence in num_lists:
-        mtable = mt(sequence,cache,gcm);
+        mtable = mt(sequence,gcm);
         uglies = defaultdict(lambda: 0);
         uglies_num = 0;
         for p in primes:

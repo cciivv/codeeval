c3013e36573a972f55e41ec77e7195cff016ab83
Carl Vanderlip <carl.i.vanderlip@gmail.com>
2015-04-01 20:59:40 -0700

    5: cycle detection working solution
    not the typical cycle detection alg since the inputs were not a graph.
    have to first determine the length of the cycle, then determine the
    location of the first cycle.

--- 5/soln.py
+++ 5/soln.py
@@ -1,2 +1,30 @@
 import sys;
 
+def find_cycle(line):
+    mu = -1;
+    for sep in range(1,len(line)):
+        tort = len(line)-1;
+        hare = tort - sep;
+        if line[hare] == line[tort]:
+            mu = sep;
+            break;
+        if mu >= 0:
+            break;
+    
+    for first in range(0,len(line)-mu):
+        if line[first] == line[first+mu]:
+            break;
+    cyc = line[first:first+mu];
+    return cyc;
+    
+def cycle_finder():
+    if len(sys.argv) <= 1:
+        print("need input file");
+    with open(sys.argv[1]) as f:
+        inputs = [line.rstrip().split() for line in f];
+    
+    output = [' '.join(find_cycle(input)) for input in inputs];
+    print("\n".join(output));
+
+if __name__ == '__main__':
+    cycle_finder()

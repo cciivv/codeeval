3dcf81f2aa6371c75b6f8ee1e268135f8969d4b8
Carl Vanderlip <carl.i.vanderlip@gmail.com>
2015-04-04 15:59:54 -0700

    187: rework part 2
    reduce dimension of problem, first by finding sequences of even numbers
    that have an existing prime loop, then by counting the number of ways
    through the possible odd values.

--- 187/soln.py
+++ 187/soln.py
@@ -15,124 +15,123 @@ primes = { 3: 0, 5: 0, 7: 0, 11: 0, 13: 0, 17: 0, 19: 0, 23: 0, 29: 0, 31: 0};
     we can simplify the process by only using values that can sum to a prime,
     will probably reach a state where 
 '''
-    
-CACHE = {};
-def prime_bead_complements(number, max):
+
+
+def prime_bead_compliments(number, max, cache):
     """ take a number and list of available numbers, and return a list of possible complimentary values"""
-    return [x for x in range(1,max+1) if x in CACHE[number]];
+    return [x for x in range(1,max+1) if x in cache[number]];
 
-class BeadNode(object):
-    def __init__(self, value, parent):
-        self.value = value;
-        self.parent = parent;
-        self.depth = parent.depth+1 if parent else 1;
-        if parent:
-            self.chain = parent.chain.copy();
-            self.chain[self.value] = self.depth;
-        else:
-            self.chain = {value: self.depth};
-        self.connected = {};
-        #print("new node ==", self);
-
-    
-    def next(self, possible):
-        return [ BeadNode(val, self) for val in possible if val not in self.chain and val in CACHE[self.value]];
-            
-    def __str__(self):
-        parent_value = self.parent.value if self.parent else 0;
-        return "[{} (from {}) depth {}-- chain {}, connected to {}]".format(repr(self.value),parent_value,repr(self.depth), self.chain, self.connected);
-
-    def __repr__(self):
-        return "{}@depth={}, chain = {}".format(self.value,self.depth,self.chain);
-        
-    def __hash__(self):
-        parent_value = self.parent.value if self.parent else 0;
-        return hash((self.value,self.depth,parent_value));
-
-
-class BeadTree(object):
-    def __init__(self, root_value, goal_depth):
-        self.depth = 1;
-        self.goal_depth = goal_depth;
-        self.head = BeadNode(root_value, None);
-        self.depth_members = defaultdict(lambda: []);
-        self.depth_members[1] = [self.head];
-        self.complete_lock = threading.Lock();
-        self.completed = 0;
-        self.work_lock = threading.Lock();
-        self.workqueue = [self.head];
-        
-
-    def grow_bud(self, bud):
-        if bud.depth == self.goal_depth and bud in CACHE[self.head.value]:
-                #print("-=-=-=-=-Connected to ", bead);
-                self.complete_lock.acquire();
-                #print("-=-=-=-=-incr-=-=-=-=-");
-                self.completed += 1;
-                self.complete_lock.release();
-        else:
-            self.work_lock.acquire();
-            self.workqueue.extend(bud.next([x for x in range(1,self.goal_depth+1)]));
-            self.work_lock.release();
-        
-            
-
-    def grow(self):
-        threads = [];
-        results = [];
-        
-        print(self.workqueue);
-        while self.workqueue:
-            bud = self.workqueue.pop();
-            self.workqueue.extend(self.grow_bud(bud));
-    def get_num_finished(self):
-        return self.completed;
-
-
-class Necklace(object):
-    def __init__(self, beads):
-        self.length = beads;
-        self.tree = BeadTree(beads, beads);
-        self.completed = 0;
-        
-        
-    def build(self):
-        ''' running for len should get all connections''' 
-        for _ in range(1, self.length):
-            self.tree.grow();
-        self.completed += self.tree.get_num_finished();
-    def __str__(self):
-        pass;
-
-
-def prime_compliment_cache():
-    for n in range(1,19):
-        if n not in CACHE:
-            CACHE[n] = {};
-            for c in [x for x in range(1,19) if x != n and x%2 != n%2]:
+def prime_compliment_cache(cache):
+    if not cache:
+        for n in range(1,19):
+            cache[n] = {};
+            for c in (x for x in range(1,19) if x != n and x%2 != n%2):
                 if (n + c) in primes:
-                    CACHE[n][c] = 1;
+                    cache[n][c] = 1;
+        #print(cache);
+
+
+def get_shared_prime_compliments(a, b, cache, range):
+    input = (a,b) if b>a else (b,a);
+    if (input,range) not in cache:
+           cache[(input,range)] = [x for x in cache[input[0]] if x <= range and x in cache[input[1]]];
+    return cache[(input,range)];
+
+
+def extend_prime_gap(last_value, value, max_value, seen, cache):
+    last = get_shared_prime_compliments(last_value, value, cache, max_value);
+    if last:
+        seen['build'].append(last);
+        seen['length'] += 1;
+        seen['last'] = value;
+        seen['sequence'].append(value);
+        seen[value] = 1;
         
-def num_patterns(length):
+        return True;
+    else:
+        return False;
+
+
+def prime_gap_helper(max_value, values, seen, cache):
+    seq_len = seen['length'];
+    last_value = seen['last'];
+    if seq_len == max_value/2 - 1:
+        if extend_prime_gap(last_value, max_value, max_value, seen, cache):
+            seen['done'].append(seen['build']);
+    else:
+        for number in [x for x in values if x not in seen]:
+            if extend_prime_gap(last_value, number, max_value, seen, cache):
+                prime_gap_helper(max_value, values, seen, cache);
+                seen.pop(number);
+                seen['last'] = seen['sequence'].pop();
+                seen['build'].pop();
+    return seen['done'];
+
+
+def get_prime_gapped_permutations(values, cache):
+    tail = values.pop();
+    seen = {tail: 1, 'last': tail, 'length':1, 'sequence':[tail], 'build': [], 'done': []};
+    done = [];
+    return prime_gap_helper(tail, values, seen, cache);
+
+
+def popualte_histogram(gaps):
+    histogram = defaultdict(lambda: 0);
+    for gap in gaps:
+        for value in gap:
+            histogram[value] += 1;
+    return histogram;
+
+
+def remove_singletons(gaps):
+    targets = defaultdict(lambda:1);
+    for gap in gaps:
+        if len(gap) == 1:
+            targets[gap[0]];
+        
+    for idx, gap in enumerate(gaps):
+        gaps[idx] = [x for x in gap if x not in targets];
+
+
+def num_ways(gaps, values_seen):
+    if len(gaps) < 1:
+        return -1;
+    if len(gaps) == 1:
+        return len([x for x in gaps[0] if x not in values_seen]);
+    ways = 0;
+    for num in (v for v in gaps[0] if v not in values_seen):
+            values_seen[num] = 1;
+            ways += num_ways(gaps[1:], values_seen);
+            values_seen.pop(num);
+    return ways;
+
+def num_odd_fills(gaps):
+        #know there is going to be at least one since get_prime_permutations
+        #only returns possible even sequences
+        remove_singletons(gaps);
+        return num_ways(gaps, {});
+    
+def num_patterns(length, cache):
     #odd numbers can't make proper value since there will always be an even number sum
     if length%2:
         return 0;
 
+    #build the cache if it hasn't been already
+    prime_compliment_cache(cache);
+    
     #print("===================START=", length);
-    string = Necklace(length);
-    string.build();
-    return string.completed;
-
+    evens = [x for x in range(1,length+1) if not x%2];
+    return sum( map( num_odd_fills, get_prime_gapped_permutations(evens, cache)));
 
 def parse_input():
     if len(sys.argv) <= 1:
         print("need input file argument");
     with open(sys.argv[1]) as f:
         lines = [ int(line.rstrip()) for line in f];
-    
-    prime_compliment_cache();
-    
-    print("\n".join([ str(num_patterns(line)) for line in lines]));
+        
+    cache = {};
+
+    print("\n".join([ str(num_patterns(line,cache)) for line in lines]));
     #print(CACHE[1]);
 
 if __name__ == '__main__':

d965b2ecb96ca56d728ab18dc0fb6e9463b4a790
Carl Vanderlip <carl.i.vanderlip@gmail.com>
2015-04-05 14:27:50 -0700

    187: more aggressive unique culling
    don't have to calculate if  the set doesn't include all numbers and if
    the reduced set of gaps contains a set more times than the number of its
    elements.
    
    possible that all this just increased running time thought :/

--- 187/soln.py
+++ 187/soln.py
@@ -1,5 +1,6 @@
 import sys;
 from collections import defaultdict;
+import time;
 
 #since input is always less than 18, max sum = 35
 #below are all primes less than 35
@@ -34,7 +35,9 @@ def get_shared_prime_compliments(a, b, cache, range):
     input = (a,b) if b>a else (b,a);
     #print("testing prime compliments of ", input);
     if (input,range) not in cache:
-        cache[(input,range)] = [x for x in cache[input[0]] if x <= range and x in cache[input[1]]];
+        comp = [x for x in cache[input[0]] if x <= range and x in cache[input[1]]];
+        comp.sort();
+        cache[(input,range)] = tuple(comp);
     #print("{} and {} (when max is {}), share compliments = {}".format(a,b,range,cache[(input,range)]));
     return cache[(input,range)];
 
@@ -118,13 +121,18 @@ def remove_singletons(gaps):
     targets = defaultdict(lambda:1);
     for gap in gaps:
         if len(gap) == 1:
+            if gap[0] in targets:
+                return (False, {});
             targets[gap[0]];
     
     for idx, gap in enumerate(gaps):
         gaps[idx] = [x for x in gap if x not in targets];
+    return (True, targets);
 
 def num_ways(gaps, values_seen):
-    #print("gaps = ", gaps, "values seen=", values_seen,"len", len(gaps));
+    #print("gaps = ", gaps, "values seen=", values_seen, "len", len(gaps));
+    if len(gaps) < 1:
+        return 1;
     if len(gaps) == 1:
         return len([x for x in gaps[0] if x not in values_seen]);
     ways = 0;
@@ -134,25 +142,111 @@ def num_ways(gaps, values_seen):
             values_seen.pop(num);
     return ways;
 
-def unique(gaps):
-    return not gaps or max([len(gap) for gap in gaps]) == 1
+def uniques_exist(gaps):
+    if not gaps:
+        return False;
+    for x in gaps:
+        if len(x) == 1:
+            return True;
+    #return len(min(gaps, key=len)) == 1
+    return False;
 
-def num_odd_fills(gaps):
-        if not unique(gaps):
-            remove_singletons(gaps);
-        return num_ways(list(filter(bool,gaps)), {});
+def no_path_exists(gaps, all_removed, max_value):
+
+    global num_no_path;
+    global num_multi;
+    needed = (max_value+1)//2 - len(all_removed);
+    len_dict = defaultdict(lambda: defaultdict(lambda: 0));
+    if not needed:
+        return False;
+    for gap in gaps:
+        tgap = tuple(gap);
+        len_dict[len(gap)][tgap] += 1;
+        if needed:
+            for n in gap:
+                if n not in all_removed:
+                    all_removed[n] = 1;
+                    needed -= 1;
+    if needed:
+        num_no_path += 1;
+        return True;
+
+    for length in len_dict:
+        for set in len_dict[length]:
+            if len_dict[length][set] > length:
+                #print(len_dict[length]);
+                num_multi += 1;
+                return True;
+    
+    return False;
+    
+num_double_single = 0;
+num_no_path = 0;
+num_multi = 0;
+def num_odd_fills(gaps, max_value):
+    global num_double_single;
+    idx = 0;
+    #print(gaps);
+    no_double_singletons = True;
+    all_removed = {};
+    while uniques_exist(gaps) and no_double_singletons:
+        #print("\t",gaps);
+        no_double_singletons, removed = remove_singletons(gaps);
+        all_removed.update(removed);
+    
+    
+    gaps = list(filter(bool,gaps));
+    if no_path_exists(gaps, all_removed, max_value):
+        if not no_double_singletons:
+            num_double_single += 1;
+        return 0;
+    
+    #print("then", gaps, "no_double_singletons =", no_double_singletons);
+    
+    if no_double_singletons:
+        if len(gaps) == 0:
+            return 1;
+        gaps.sort(key=len);
+        result = num_ways(gaps, {});
+        #if not result:
+        #    print("zero gap = ",gaps, "\n");
+        return result;
+    else:
+        num_double_single += 1; 
+        return 0;
     
 def num_patterns(length, cache):
     #odd numbers can't make proper value since there will always be an even number sum
+    
+    global num_double_single;
+    global num_no_path;
+    global num_multi;
     if length%2:
         return 0;
 
     #build the cache if it hasn't been already
     prime_compliment_cache(cache);
-    
+    print("start", length);
     evens = [x for x in range(1,length+1) if not x%2];
+    t = time.time();
     gaps = get_prime_gapped_permutations(evens, cache);
-    return sum( map( num_odd_fills, gaps));
+    print(len(gaps));
+    num_double_single = 0;
+    num_no_path = 0;
+    num_multi = 0;
+    max_per_gap = defaultdict(lambda: 0);
+    result = 0;
+    for gap in gaps:
+        temp = num_odd_fills(gap,length);
+        max_per_gap[temp]+=1;
+        result += temp;
+    #result = sum( map( num_odd_fills, gaps));
+    print("time = ", (time.time() - t));
+    print("bad gaps", num_double_single);
+    print("no path gaps", num_no_path);
+    print("no multi", num_multi);
+    print("gap result distribution",max_per_gap);
+    return result;
 
 def parse_input():
     if len(sys.argv) <= 1:
